7bd3bdc7291d9186261e961a2f4031951287aab3
megha <meghamini40@gmail.com>
2022-08-12 11:02:17 +0530

    create a seperate folder for routers

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { fetchAllDetails, uploadImage, updateDetails, findImage } = require('../controller/actions');
+const { fetchAllDetails, uploadImage, updateDetails, findImage, findByIdAndRemove } = require('../controller/actions');
 
 const Router = express.Router();
 
+Router.get('/', (req, res) => res.send("let starts"))
 Router.get('/getAllDetails', fetchAllDetails);
-Router.get('/findImage', findImage)
+Router.get('/findImage/:id', findImage)
 Router.post('/upload', uploadImage)
 Router.put('/updateDetails/:id', updateDetails)
-
+Router.delete('/findByIdAndRemove/:id', findByIdAndRemove)
+// Router.delete('/deleteMany', deleteMany)
 
 module.exports = Router;
