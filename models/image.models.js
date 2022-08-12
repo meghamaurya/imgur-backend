bd30cf1801bf689d0dd9e01b31c15998759dad36
megha <meghamini40@gmail.com>
2022-08-12 11:03:28 +0530

    create model for image

--- models/image.models.js
+++ models/image.models.js
@@ -1,16 +1,45 @@
 const mongoose = require('mongoose');
 
-const imageSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    image: {
-        data: Buffer,
-        contentType: String
-    }
+var imageSchema = new mongoose.Schema({
+    username: String,
+    image: String,
+    Detail: String,
+    // data: Buffer,
+    contentType: String,
 }, {
     timeStamps: true
+});
+imageSchema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
 })
 
-module.exports = ImageModel = mongoose.model("Image", imageSchema);
+module.exports = mongoose.model("Image", imageSchema);
+
+
+
+
+
+/* to use multer
+ const mongoose = require('mongoose');
+    var imageSchema = mongoose.Schema({
+        name: {
+            type: String,
+            required: true
+        },
+        image: {
+            data: Buffer,
+            contentType: String
+        }
+    }, {
+        timeStamps: true
+    });
+    imageSchema.method("toJSON", function () {
+        const { __v, _id, object } = this.toObject();
+        object.id = _id;
+        return object;
+    })
+
+    module.export = ImageModel = mongoose.model("Image", imageSchema);
+   */
